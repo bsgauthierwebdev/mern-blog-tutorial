4f81a1699e84b71e99403fe2834a0c734abbb75b
Brent Gauthier <cajundrummer@gmail.com>
2024-05-11 16:19:57 -0400

    Build & design Posts display area

--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -1,9 +1,16 @@
+import Post from '../Post/Post'
 import './Posts.css'
 
 export default function Posts() {
   return (
     <div className="posts">
-        Posts
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </div>
   )
 }
