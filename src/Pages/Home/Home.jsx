69970b1277289013ee480ee4762a3c5323eab665
Brent Gauthier <cajundrummer@gmail.com>
2024-05-11 15:46:01 -0400

    Import Sidebar & Posts into Home Page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import Header from '../../Components/Header/Header'
+import Posts from '../../Components/Posts/Posts'
+import Sidebar from '../../Components/Sidebar/Sidebar'
 import './Home.css'
 
 export default function Home() {
   return (
-    <div className="home">
-        <Header />
-        Home Page
-    </div>
+    <>
+      <Header />
+      <div className="home">
+        <Posts />
+        <Sidebar />
+      </div>
+    </>
   )
 }
