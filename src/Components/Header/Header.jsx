1aa92c75e62404cc52d8581f7e97e0d2ca946d2f
Brent Gauthier <cajundrummer@gmail.com>
2024-05-12 20:39:03 -0400

    Change font to italic

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <div className="header">
         <div className="headertitles">
-            <span className = 'headertitlesmall'>React & Node</span>
-            <span className = 'headertitlelarge'>Blog</span>
+            <span className = 'headertitlesmall'><i>React & Node</i></span>
+            <span className = 'headertitlelarge'><i>Blog</i></span>
         </div>
         <img 
             src="https://t3.ftcdn.net/jpg/02/00/00/14/360_F_200001420_oc3FTjnYScErU0kn52shLhmeXa2BtkzX.jpg" 
