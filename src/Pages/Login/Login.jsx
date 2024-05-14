700977f24ac06423da03e03a22e03e85f301280b
Brent Gauthier <cajundrummer@gmail.com>
2024-05-13 21:43:54 -0400

    Update placeholder text

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -6,9 +6,17 @@ export default function Login() {
         <span className="logintitle">Login</span>
         <form className="loginform">
             <label>Email</label>
-            <input type="text" className="logininput" placeholder = 'Email' />
+            <input 
+              type="text" 
+              className="logininput" 
+              placeholder = 'Enter your email address...' 
+            />
             <label>Password</label>
-            <input type="password" className="logininput" placeholder = 'Password' />
+            <input 
+              type="password" 
+              className="logininput" 
+              placeholder = 'Enter your password...' 
+            />
             <button className="loginbutton">Login</button>
         </form>
         <button className="loginregisterbutton">Register</button>
