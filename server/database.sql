d09a8b03b161aa00909c3832bd06958302a0da9c
Brent Gauthier <cajundrummer@gmail.com>
2024-06-03 21:19:37 -0400

    Finish schema for all three tables: Users, Posts & Categories

--- server/database.sql
+++ server/database.sql
@@ -1,9 +1,31 @@
 -- Users Table
 
 CREATE TABLE users (
-    user_id: UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    username: VARCHAR(255) UNIQUE NOT NULL,
-    email: VARCHAR(255) UNIQUE NOT NULL,
-    password: VARCHAR(255) NOT NULL,
-    profile_picture_filename TEXT
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    profile_picture_filename TEXT,
+    created_at DATE DEFAULT current_date
+)
+
+-- Posts Table
+
+CREATE TABLE posts (
+    post_id SERIAL PRIMARY KEY,
+    username VARCHAR(255),
+    title VARCHAR(500) UNIQUE NOT NULL,
+    content TEXT NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    post_img_filename TEXT,
+    category VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURERNT_TIMESTAMP,
+    FOREIGN KEY (username) REFERENCES users (username),
+    FOREIGN KEY (category) REFERENCES categories (category)
+)
+
+-- Category Table
+
+CREATE TABLE categories (
+    category VARCHAR(255) UNIQUE NOT NULL
 )
