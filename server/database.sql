cb8b29ec8c1aa1ad76e9e5380fba256d62365756
Brent Gauthier <cajundrummer@gmail.com>
2024-06-11 19:27:58 -0400

    Restructure schemas

--- server/database.sql
+++ server/database.sql
@@ -1,13 +1,20 @@
 -- Users Table
 
 CREATE TABLE users (
-    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    profile_picture_filename TEXT,
+    profile_picture_filename TEXT DEFAULT NULL,
     created_at DATE DEFAULT current_date
-)
+);
+
+-- Category Table
+
+CREATE TABLE categories (
+    catgory_id SERIAL PRIMARY KEY,
+    category VARCHAR(255) UNIQUE NOT NULL
+);
 
 -- Posts Table
 
@@ -19,13 +26,7 @@ CREATE TABLE posts (
     description VARCHAR(1000) NOT NULL,
     post_img_filename TEXT,
     category VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURERNT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (username) REFERENCES users (username),
     FOREIGN KEY (category) REFERENCES categories (category)
-)
-
--- Category Table
-
-CREATE TABLE categories (
-    category VARCHAR(255) UNIQUE NOT NULL
-)
+);
